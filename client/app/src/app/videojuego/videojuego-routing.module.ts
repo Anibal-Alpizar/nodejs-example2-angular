0521356d1ee625069831a2af3a7ba213bbf4e244
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-06-13 16:32:37 -0600

    videojuego

--- client/app/src/app/videojuego/videojuego-routing.module.ts
+++ client/app/src/app/videojuego/videojuego-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { VideojuegoIndexComponent } from './videojuego-index/videojuego-index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "videojuego", component: VideojuegoIndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- client/app/src/app/videojuego/videojuego.module.ts
+++ client/app/src/app/videojuego/videojuego.module.ts
@@ -2,12 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VideojuegoRoutingModule } from './videojuego-routing.module';
+import { VideojuegoIndexComponent } from './videojuego-index/videojuego-index.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatMenuModule } from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatDividerModule} from '@angular/material/divider';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VideojuegoIndexComponent
+  ],
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatGridListModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
+
     VideojuegoRoutingModule
   ]
 })
